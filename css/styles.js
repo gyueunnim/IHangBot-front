f0b3077a583c57cf8b181bc9c8a73a7043fbe59b
gyueunnim <rnrmfrbdms@gmail.com>
2023-08-01 15:18:36 +0900

    design: 임시 디자인 적용

---
+++ css/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+    },
+    content: {
+      flex: 1,
+    },
+    text: {
+      fontSize: 30,
+    },
+    bottomTextContainer: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      padding: 16,
+    },
+    bottomText: {
+      fontSize: 18,
+      textAlign: 'center',
+    },
+  });
