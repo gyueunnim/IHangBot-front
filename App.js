7b64fdc1609488e670696ddfa7675e40293fa31e
gyueunnim <rnrmfrbdms@gmail.com>
2023-08-01 13:17:15 +0900

    chore: add routing settings

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    </View>
+    <NavigationContainer>
+    </NavigationContainer>
   );
 }
 
