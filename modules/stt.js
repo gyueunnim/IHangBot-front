f9fa20c2867d57ea4adc77d3a6fc66b2b7838207
gyueunnim <rnrmfrbdms@gmail.com>
2023-08-02 17:50:08 +0900

    feat: speech to text 구현

---
+++ modules/stt.js
@@ -0,0 +1,40 @@
+import * as FileSystem from 'expo-file-system';
+import axios from 'axios';
+import { toByteArray } from 'base64-js';
+
+const clientId = '';
+const clientSecret = '';
+const url = 'https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=Kor';
+
+export default async function stt(fileUri) {
+	try {
+		const fileInfo = await FileSystem.getInfoAsync(fileUri);
+
+		if (fileInfo.exists) {
+			const fileContent = await FileSystem.readAsStringAsync(fileUri, {
+				encoding: FileSystem.EncodingType.Base64
+			});
+
+			// 디코딩
+			const binaryData = toByteArray(fileContent);
+
+			try {
+				const response = await axios
+				.post(url, binaryData, {
+					headers: {
+						'Content-Type': 'application/octet-stream',
+						'X-NCP-APIGW-API-KEY-ID': clientId,
+						'X-NCP-APIGW-API-KEY': clientSecret,
+					}
+				});
+				return response.data;
+			} catch (error) {
+				console.error(error);
+				return { text: error };
+			}
+		}
+	} catch (error) {
+			console.log(error);
+			return { text: error };
+	}
+}
