0305c9b7fc8da5f7ca4d7e9d68f3a948d194b482
gyueunnim <rnrmfrbdms@gmail.com>
2023-08-03 10:28:44 +0900

    feat: loginInfo redux 추가

---
+++ redux/logInfo.js
@@ -0,0 +1,25 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const loginInitialState = {
+    id: '',
+    login: false,
+}
+
+const loginState = createSlice({
+    name: 'loginState',
+    initialState: loginInitialState,
+    reducers: {
+        setLoginState(state, action) {
+            state.id = action.payload;
+            state.login = true;
+        }
+    }
+})
+
+export default configureStore({
+    reducer: {
+        loginState: loginState.reducer
+    }
+});
+
+export const { setLoginState } = loginState.actions;
