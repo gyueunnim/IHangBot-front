b239a3652a948886c2042c52659eb857811f1571
gyueunnim <rnrmfrbdms@gmail.com>
2023-08-03 10:30:34 +0900

    feat: redux 추가 및 navigation stack 재설정

--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,20 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions, Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { setLoginState } from '../redux/logInfo';
+import { CommonActions } from '@react-navigation/native';
 
 function Login({navigation}) {
+  const loginState = useSelector((state) => state.loginState);
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
   const [btnStyle, setBtnStyle] = useState(loginStyle.btn);
   const [loginErr, setLoginErr] = useState(false);
+  const [defaultInput, setDefaultInput] = useState(null);
+  const [screen, setScreen] = useState('ChatBot');
+
+  const dispatch = useDispatch();
 
   const loginInfo = {
     "username": id, 
@@ -20,11 +28,20 @@ function Login({navigation}) {
     setLoginErr(false);
   }, [id, pw])
 
-  // TODO: 서버 통신
+  useEffect(() => {
+    if(loginState.login === true) {
+      setId(loginState.id);
+      setDefaultInput(loginState.id);
+    } else {
+      setDefaultInput(null);
+    }
+  }, [])
+
   const requestLogin = () => {
     axios.get(`http://52.79.225.144:8080/member/login`, { params: loginInfo })
       .then((response) => {
         if(response.data.status == 200) {
+          setPw('');
           successLogin();
         } else {
           console.log(response);
@@ -32,30 +49,50 @@ function Login({navigation}) {
         }
       })
       .catch((error) => {
+        console.log(error)
         setLoginErr(true);
       })
   }
 
   const successLogin = () => {
-    setLoginErr(false);  
-    navigation.navigate('Main');
-  }
+    setLoginErr(false);
+    if(loginState.login === false) {
+      dispatch(setLoginState(id));
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 1, 
+          routes: [
+            { name: 'ChatBot' },
+          ],
+        })
+      );
+    } else {
+      navigation.dispatch(CommonActions.navigate('Report'))
+    }
+  };
     
   return (
     <View style={loginStyle.container}>
       <View>
         <Image source={require('../assets/mainLogo.png')} style={loginStyle.icon}></Image>
-        <Text style={loginStyle.iconText}>로그인이 필요한 서비스입니다</Text>
+        {
+          loginState.login === false ? <Text style={loginStyle.iconText}>로그인이 필요한 서비스입니다</Text>
+          : <Text style={loginStyle.goReport}>보고서 화면으로 이동합니다</Text>
+        }
         <View style={{flexDirection: 'row'}}>
           <Text style={loginStyle.title}>아이디</Text>
           {
-            loginErr === true ? <Text style={loginStyle.loginError}>로그인에 실패하였습니다.</Text>
-            : null
+            loginErr === true ? <Text style={loginStyle.loginError}>로그인에 실패하였습니다</Text> : null
           }
         </View>
-        <TextInput placeholder="아이디를 입력하세요" onChangeText={(value) => setId(value)} style={loginStyle.form} />
+        <TextInput placeholder="아이디를 입력하세요" onChangeText={(value) => setId(value)} defaultValue={defaultInput} style={loginStyle.form} />
+        <View style={{flexDirection: 'row'}}>
         <Text style={loginStyle.title}>비밀번호</Text>
-        <TextInput placeholder="비밀번호를 입력하세요" onChangeText={(value) => setPw(value)} secureTextEntry={true} style={loginStyle.form} />
+        {
+          loginState.login === false ? null : (pw === '' ? <Text style={loginStyle.goReportText}>비밀번호를 입력해주세요</Text> : null)
+        }
+        </View>
+        <TextInput placeholder="비밀번호를 입력하세요" onChangeText={(value) => setPw(value)} defaultValue={null}secureTextEntry={true} style={loginStyle.form} />
       </View>
       <View>
         <TouchableOpacity onPress={() => requestLogin()}>
@@ -83,10 +120,10 @@ const loginStyle = StyleSheet.create({
   },
   iconText: {
     textAlign: 'center',
-    padding: 25,
-    paddingBottom: 0,
+    padding: 20,
+    paddingBottom: 15,
     fontWeight: 'bold', 
-    fontSize: 10,
+    fontSize: 12,
     color: '#3f6ad7',
   },
   container: {
@@ -147,6 +184,25 @@ const loginStyle = StyleSheet.create({
     paddingBottom: 2.5,
     marginTop: 5,
     color: '#cc0000',
+    fontWeight: 'bold',
+    textAlign: 'right',
+  },
+  goReport: {
+    textAlign: 'center',
+    padding: 25,
+    paddingBottom: 0,
+    fontWeight: 'bold', 
+    fontSize: 12,
+    color: '#3f6ad7',
+    fontWeight: 'bold',
+  },
+  goReportText: {
+    flex: 1,
+    padding: 10,
+    paddingLeft: 0,
+    paddingBottom: 2.5,
+    marginTop: 5,
+    color: '#528638',
     fontWeight: 'bold',
     textAlign: 'right',
   },
