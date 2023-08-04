acc517913db1c3d909302330716af87f8a3497b8
gyueunnim <rnrmfrbdms@gmail.com>
2023-08-04 14:52:58 +0900

    fix: 환경변수 임시 수정

--- modules/tts.js
+++ modules/tts.js
@@ -2,8 +2,8 @@ import * as FileSystem from 'expo-file-system';
 import axios from 'axios';
 import qs from 'qs';
 
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
+const clientId = 'r6yms009vl';
+const clientSecret = 'vSGFZ0fpSUaRqfyE2xETvbfzrG6V3QCSyvRhg8Hn';
 const url = 'https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts';
 
 export default async function tts(chatResponse) {
