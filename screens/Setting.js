670f5681fd7ea02338e11418e16b38f3d5499127
gyueunnim <rnrmfrbdms@gmail.com>
2023-08-04 15:52:33 +0900

    feat: 프로필 수정 서버 임시 연동 코드 추가 및 성별 수정 추가

--- screens/Setting.js
+++ screens/Setting.js
@@ -4,21 +4,54 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import { styles } from '../css/reportSetStyles';
 import { loginStyle } from '../css/loginStyles';
 import { signUpStyle } from '../css/signUpStyles';
+import { useSelector } from 'react-redux';
+import { RadioButton } from 'react-native-paper';
+import axios from 'axios';
 
 function Setting({navigation}) {
+  const loginState = useSelector((state) => state.loginState);
+
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
   const [pw, setPw] = useState('');
   const [pwCheck, setPwCheck] = useState('');
+  const [gender, setGender] = useState(true);
   const [btnStyle, setBtnStyle] = useState(loginStyle.btn);
   const [pwCheckErr, setPwCheckErr] = useState(0);
+  const [userInfo, setUserInfo] = useState({child_name: '', age: ''});
+  
+  const requestSetting = () => {
+    axios.put(`http://52.79.225.144:8080/member/${loginState.id}/profile/setting`, {    
+      "child_name": "string",
+      "child_age": 0,
+      "child_gender": true,
+      "email": "string"
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => console.log(error));
+  };
 
   useEffect(() => {
     pwCheck !== '' ? (pw !== pwCheck ? setPwCheckErr(1)  : setPwCheckErr(2)) 
     : setPwCheckErr(0);
-    console.log(pw, pwCheck);
-    console.log(pwCheckErr);
-  }, [pw, pwCheck])
+  }, [pw, pwCheck]);
+
+  // useEffect(() => {
+  //   console.log(pw, pwCheck, name, gender, age)
+  // })
+
+  useEffect(() => {
+    axios.get(`http://52.79.225.144:8080/member/${loginState.id}/profile`)
+      .then((response) => {
+        setUserInfo({
+          child_name: response.data.data.child_name,
+          age: response.data.data.child_age
+        });
+      })
+      .catch((error) => console.log(error));
+  }, [])
 
   return (
     <View style={styles.container}>
@@ -30,12 +63,36 @@ function Setting({navigation}) {
       <View style={styles.middle}>
         <View>
           <Text style={styles.title}>아이 이름</Text>
-          <TextInput placeholder="아이 이름을 입력하세요" onChangeText={(value) => setName(value)} style={styles.form} />
+          <TextInput placeholder="아이 이름을 입력하세요" defaultValue={userInfo.child_name} onChangeText={(value) => setName(value)} style={styles.form} />
         </View>
-        <View>
-          <Text style={styles.title}>나이</Text>
-          <TextInput placeholder="나이를 입력하세요" onChangeText={(value) => setAge(value)} style={styles.form} />
+
+        <View style={signUpStyle.fx}>
+          <View>
+            <View style={signUpStyle.fx}>
+              <Text style={signUpStyle.title}>성별 </Text>
+              <Text style={signUpStyle.highlight}>*</Text>
+            </View>
+            <View style={signUpStyle.fx}>
+              <View style={signUpStyle.fx}>
+                <RadioButton status={gender === true ? "checked" : "unchecked"} onPress={() => setGender(true)} />
+                <Text style={signUpStyle.radioText}>남자</Text>
+              </View>
+              <View style={signUpStyle.fx}>
+                <RadioButton status={gender == false ? "checked" : "unchecked"} onPress={() => setGender(false)} />
+                <Text style={signUpStyle.radioText}>여자</Text>
+              </View>
+            </View>
+          </View>
+
+          <View>
+            <View style={signUpStyle.formAge}>
+              <Text style={signUpStyle.title}>나이 </Text>
+              <Text style={signUpStyle.highlight}>*</Text>
+            </View>
+            <TextInput placeholder="아이의 나이" defaultValue={userInfo.age.toString()} onChangeText={(value) => setAge(value)} style={signUpStyle.fxForm} />
+          </View>  
         </View>
+
         <View>
           <Text style={styles.title}>비밀번호 변경</Text>
           <TextInput placeholder="변경하실 비밀번호를 입력하세요" secureTextEntry={true} onChangeText={(value) => setPw(value)} style={styles.form} />
